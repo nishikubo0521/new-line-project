8f46be2a0ad09d8fe1c5fcc116f06013ab9d6a66
nishikubo0521 <nishikubo0521@gmail.com>
2016-11-28 00:58:05 +0900

    ver 1

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
+var async = require('async');
+var crypto = require('crypto');
+var request = require('request');
 
 // port number
 var port = process.env.PORT || 3000;
@@ -25,9 +28,39 @@ app.get('/', function(req, res){
 
 // A router to send answers by json post
 app.post('/message', function(req, res){
-	// console.log(req.body);
-  io.emit('chat message', 'LINEからなんかきた！');
-	res.status(200).end();
+
+		// リクエストがLINE Platformから送られてきたか確認する
+		if (!validate_signature(req.headers['x-line-signature'], req.body)) {
+			io.emit('chat message', 'LINEじゃないよ！');
+			return;
+		}
+
+		// テキストが送られてきた場合のみ返事をする
+		if ((req.body['events'][0]['type'] != 'message') || (req.body['events'][0]['message']['type'] != 'text')) {
+			io.emit('chat message', 'テキストはないよ！');
+			return;
+		}
+
+		// テキスト送信元のユーザーIDを取得する。
+		var userId = req.body['events'][0]['source']['userId'];
+
+		var options = {
+		  url: 'https://api.line.me/v2/bot/profile/' + userId,
+		  proxy: process.env.FIXIE_URL,
+      json: true,
+		  headers: {
+		    'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}'
+		  }
+		};
+
+		// ユーザー情報を取得する。
+		request.get(options, function(error, response, body){
+			if (!error && response.statusCode == 200) {
+				io.emit('chat message', body['displayName']);
+				res.status(200).end();
+			}
+		});
+
 });
 
 // socket.io to connect this server with answer view
@@ -41,3 +74,8 @@ io.on('connection', function(socket){
 http.listen(port, function(){
 	console.log('listening to port:' + port);
 });
+
+// 署名検証
+function validate_signature(signature, body) {
+    return signature == crypto.createHmac('sha256', process.env.LINE_CHANNEL_SECRET).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
+}
