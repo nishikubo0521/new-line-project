959291d149dfd62e89382f34e4f8517184589087
nishikubo0521 <nishikubo0521@gmail.com>
2016-11-27 23:16:18 +0900

    added async

--- server.js
+++ server.js
@@ -2,29 +2,11 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-// var https = require('https').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
-var LINEBot = require('line-messaging');
 
 // port number
 var port = process.env.PORT || 3000;
-
-// LINE bot instance
-// var bot = LINEBot.create({
-//   channelID: '1489062968',
-//   channelSecret: '71e1c7bf525ace35ac89c39c45d11d7f',
-//   channelToken: 'oAMtDVspf6zqGXP+4kP9max88/w/wsnOyoecOXRNPb2YxWYt+ko0gN3JbcqdX+OhDzajL/l7Qie8+eU3zcqO31cxNePOhjiUHDCT3EIgP6I/9ef4LnONPzVe6mOyHF5gWZ89CagcF9PFfir1L4RMIgdB04t89/1O/w1cDnyilFU='
-// }, http);
-
-// parse bot message
-// app.use(bot.webhook('/message'));
-
-// Message event
-// bot.on(LINEBot.Events.MESSAGE, function(replyToken, message) {
-//   // add code below. 
-//   io.emit('chat message', "message");
-// });
 
 var options = {
   root: __dirname,
@@ -40,11 +22,6 @@ app.use(bodyParser.json());
 app.get('/', function(req, res){
   res.sendFile('index.html', options);
 });
-
-// A test view to send answers
-// app.get('/message', function(req, res){
-// 	res.sendFile('message.html', options);
-// });
 
 // A router to send answers by json post
 app.post('/message', function(req, res){
